d98766d08f44a7b1df072f6496a9a7837582da6c
Krishna098123 <156405473+Krishna098123@users.noreply.github.com>
2024-01-15 23:46:42 +0530

    Update login.php

--- login.php
+++ login.php
@@ -3,11 +3,10 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $username = $_POST['username'];
     $password = $_POST['password'];
 
-    // Validate login credentials
-    // Perform database validation or other necessary operations here
-
     // For simplicity, check if the username and password match
-    if ($username === 'example' && password_verify($password, '$2y$10$Bt/B8/XjxJU72/y25R/7Suj8ohRDsJOJX.Ks/Z5cw/NiRLaNH/0F6')) {
+    $hashedPassword = password_hash('example_password', PASSWORD_BCRYPT); // Replace 'example_password' with the actual password used during registration
+    
+    if ($username === 'example' && password_verify($password, $hashedPassword)) {
         echo "Login successful!";
     } else {
         echo "Invalid credentials!";
